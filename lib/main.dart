19e4057c827c8e37e13fad36a7f11b7a2454c02a
ThomasNow Productions <thomasnowprod@proton.me>
2025-11-18 11:03:57 +0100

    Updated directory structure

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'LinuxDex',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.green.shade400, // Terminal green
